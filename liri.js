f321c332c5510fe09f7c97d41687c4e2c134163d
Qbisst <ismaelaperez@outlook.com>
2019-04-08 00:24:31 -0400

    some files added

---
+++ liri.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+// Variables for required NPM packages//
+var axios = require("axios");
+var keys = require("./keys");
+var moment = require("moment");
+var spotify = require("node-spotify-api");
+var request = requrire("request");
+var fs = require("fs");
+
